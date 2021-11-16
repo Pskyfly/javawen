0b2d4716cd3a3e3f4a500af584ed5a8af91c8bcb
天航 李 <1197044686@qq.com>
2021-11-16 15:51:15 +0800

    错误未解决

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,24 +2,31 @@ package com.example.demo.controller;
 
 import com.example.demo.bean.UserT;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
+
+    @Autowired
     UserService userService;
-    @RequestMapping("/login")
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String show(){
         return "login";
     }
+
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(String name,String password){
+    public UserT login(String name,String password){
         UserT userBean = userService.loginIn(name,password);
         if(userBean!=null){
-            return "content";
-        }else {
-            return "welcome";
+            UserT user=new UserT();
+            user.setPassword(password);
+            user.setName(name);
+            return user;
         }
+        return null;
     }
 }
