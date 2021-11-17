7267b74c36d8e16fcfe2b0c75cdc1fa8ceb81093
天航 李 <1197044686@qq.com>
2021-11-17 13:02:36 +0800

    登录功能完成

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.example.demo.bean.UserT;
 import com.example.demo.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -15,21 +21,34 @@ public class LoginController {
     @Resource
     UserService userService;
 
+    protected Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String show(){
         return "login";
     }
 
-    @RequestMapping(value = "/loginIn",method = RequestMethod.POST)
-    public String login(){
-//        UserT userBean = userService.loginIn(name,password);
-//        if(userBean!=null){
-//            UserT user=new UserT();
-//            user.setPassword(password);
-//            user.setName(name);
-//            return user;
-//        }
-//        return null;
-        return "content";
+    @RequestMapping(value = "/log",method = RequestMethod.POST)
+    public Object login(String name,String password){
+        //return userService.loginin(name,password).getName();
+        return "content.html";
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ResponseBody
+    public Object other(String name, String password, ModelMap modelMap){
+        UserT user = userService.loginin(name, password);
+        resultMap.clear();
+        if(user!=null) {
+            resultMap.put("status", 200);
+            resultMap.put("message", "登录成功");
+            resultMap.put("id", "" + user.getId());
+        }
+        else
+        {
+            resultMap.put("status", 500);
+            resultMap.put("message", "登录失败");
+        }
+        return resultMap;
     }
 }
